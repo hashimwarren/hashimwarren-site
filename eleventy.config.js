2ff911e3e567a3679036d1c775ea49151f557471
Hashim Warren <hashimwarren@gmail.com>
2025-05-11 23:59:12 -0400

    fix: re-enable base layout alias so contact page is generated

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,16 +1,16 @@
-module.exports = function (eleventyConfig) {
-  // Specify a different layouts directory.
-  // eleventyConfig.addLayoutAlias("base", "base.liquid"); // Removed this line
+module.exports = function(eleventyConfig) {
+  // re-add this so `layout: base` -> `_layouts/base.liquid`
+  eleventyConfig.addLayoutAlias("base", "base.liquid");
 
   return {
     dir: {
       input: ".",
       includes: "_includes",
       layouts: "_layouts",
-      output: "_site",
+      output: "_site"
     },
-    templateFormats: ["md", "liquid", "html"],
+    templateFormats: ["md","liquid","html"],
     markdownTemplateEngine: "liquid",
-    htmlTemplateEngine: "liquid",
+    htmlTemplateEngine: "liquid"
   };
 };
